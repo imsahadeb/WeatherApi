80cccb52c5360125e421337c714a761409e8954b
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-23 11:31:39 +0530

    initial commit'

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const http = require('http');
 //const functions = require('firebase-functions');
 const express =require('express');
 const bodyParser = require('body-parser');
-app = express();
+
 const host = 'api.worldweatheronline.com';
 const wwoApiKey = 'd39a3c387837421685761154180605';
 
+const {
+  dialogflow,
+  SimpleResponse,
+  BasicCard,
+  Button,
+  Carousel,
+  BrowseCarousel,
+  List,
+  Suggestions,
+  Image
+} =require('actions-on-google');
 
+const WELCOME_INTENT =                'Default Welcome Intent';
   // // Get the city and date from the request
   // let city = req.body.queryResult.parameters['geo-city']; // city is a required param
 
@@ -18,58 +30,66 @@ const wwoApiKey = 'd39a3c387837421685761154180605';
   //   console.log('Date: ' + date);
   // }
 
-  app.use('/webhook',(req,res)=>{
+  const app = dialogflow({debug:true});
+  app.intent('Default Welcome Intent',(conv)=>{
+    conv.ask(new SimpleResponse({
+      speech:"hi welcome, this is defaultelcome intent",
+      text:"hii dbfhfh"
+    }));
+    let city='kolkata';
+    let date='23-05-2018';
+  
+    // Call the weather API
+    callWeatherApi(city, date).then((output) => {
+     // res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
+     console.log(output);
+    }).catch(() => {
+    //  res.json({ 'fulfillmentText': `I don't know the weather but I hope it's good!` });
+    console.log('error');
+    });
+  
 
-  let city='kolkata';
-  let date='23-05-2018';
-
-  // Call the weather API
-  callWeatherApi(city, date).then((output) => {
-    res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
-  }).catch(() => {
-    res.json({ 'fulfillmentText': `I don't know the weather but I hope it's good!` });
-  });
-
-  });
+  })
+  
+  
 
 
 
-function callWeatherApi (city, date) {
-  return new Promise((resolve, reject) => {
-    // Create the path for the HTTP request to get the weather
-    let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
-      '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
-    console.log('API Request: ' + host + path);
-
-    // Make the HTTP request to get the weather
-    http.get({host: host, path: path}, (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => { body += d; }); // store each response chunk
-      res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
-        let response = JSON.parse(body);
-        let forecast = response['data']['weather'][0];
-        let location = response['data']['request'][0];
-        let conditions = response['data']['current_condition'][0];
-        let currentConditions = conditions['weatherDesc'][0]['value'];
-
-        // Create response
-        let output = `Current conditions in the ${location['type']} 
-        ${location['query']} are ${currentConditions} with a projected high of
-        ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
-        ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
-        ${forecast['date']}.`;
-
-        // Resolve the promise with the output text
-        console.log(output);
-        resolve(output);
-      });
-      res.on('error', (error) => {
-        console.log(`Error calling the weather API: ${error}`)
-        reject();
+  function callWeatherApi (city, date) {
+    return new Promise((resolve, reject) => {
+      // Create the path for the HTTP request to get the weather
+      let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
+        '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
+      console.log('API Request: ' + host + path);
+  
+      // Make the HTTP request to get the weather
+      http.get({host: host, path: path}, (res) => {
+        let body = ''; // var to store the response chunks
+        res.on('data', (d) => { body += d; }); // store each response chunk
+        res.on('end', () => {
+          // After all the data has been received parse the JSON for desired data
+          let response = JSON.parse(body);
+          let forecast = response['data']['weather'][0];
+          let location = response['data']['request'][0];
+          let conditions = response['data']['current_condition'][0];
+          let currentConditions = conditions['weatherDesc'][0]['value'];
+  
+          // Create response
+          let output = `Current conditions in the ${location['type']} 
+          ${location['query']} are ${currentConditions} with a projected high of
+          ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
+          ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
+          ${forecast['date']}.`;
+  
+          // Resolve the promise with the output text
+          console.log(output);
+          resolve(output);
+        });
+        res.on('error', (error) => {
+          console.log(`Error calling the weather API: ${error}`)
+          reject();
+        });
       });
     });
-  });
-}
-
-app.listen(80);
+  }
+express().use(bodyParser.json(),app).listen(process.env.PORT || 80);
